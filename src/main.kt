3ee74a9b1d1367400f1d8a94468be94dd5a673fb
Reyson Barros <=>
2022-03-02 15:42:16 -0300

    Creating Account class

--- src/main.kt
+++ src/main.kt
@@ -2,22 +2,37 @@ fun main() {
 
     println("Welcome to Bytebank")
 
-    val entity = "Entity"
+    val accountJohn = Account()
+    accountJohn.owner = "John Doe"
+    accountJohn.number = 1001
+    accountJohn.balance = 300.0
+
+    val accountMaria = Account()
+    accountMaria.owner = "Maria Doe"
+    accountMaria.number = 1002
+    accountMaria.balance = 450.0
+
+    printAccountInfo(accountJohn)
+    printAccountInfo(accountMaria)
+
+
+    /*val entity = "Entity"
     val account = 12100886
     var amount = 0.0
     amount += 200
 
     printAccountInfo(entity,account,amount)
     testConditions(amount)
-    testLoops(entity,account,amount)
+    testLoops(entity,account,amount)*/
 
 
 }
 
-fun printAccountInfo(entity: String, account: Int, amount: Double){
-    println("entity $entity")
-    println("account $account")
-    println("amount $amount")
+fun printAccountInfo(account: Account){
+    println("owner ${account.owner}")
+    println("account number ${account.number}")
+    println("balance ${account.balance}")
+    println()
 }
 
 fun testConditions(amount: Double){
@@ -67,4 +82,10 @@ fun testLoops(entity: String, account: Int, amount: Double){
         println("amount ${amount+contador}")
         contador++
     }
+}
+
+class Account{
+    var owner = ""
+    var number = 1000
+    var balance = 0.0
 }
