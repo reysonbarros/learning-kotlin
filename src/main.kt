9ad354e2945237f0d83fdb72437e08a484b703d2
Reyson Barros <=>
2022-03-02 21:00:25 -0300

    Implementing transfer function

--- src/main.kt
+++ src/main.kt
@@ -1,31 +1,42 @@
 fun main() {
 
     println("Welcome to Bytebank")
+    println()
 
     val accountJohn = Account()
     accountJohn.owner = "John Doe"
     accountJohn.number = 1001
-    accountJohn.balance = 300.0
+    accountJohn.balance = 100.0
 
     val accountMaria = Account()
     accountMaria.owner = "Maria Doe"
     accountMaria.number = 1002
-    accountMaria.balance = 450.0
+    accountMaria.balance = 200.0
 
-    printAccountInfo(accountJohn)
-    accountJohn.deposit(15.0)
-    accountJohn.withdraw(80.0)
-    accountJohn.withdraw(800.0)
-    println("balance ${accountJohn.balance}")
+    println("Balance Account - John")
+    println("Initial balance ${accountJohn.balance}")
+    accountJohn.deposit(10.0)
+    accountJohn.withdraw(20.0)
+    if(accountJohn.transfer(30.0, accountMaria)){
+        println("Transfer was completed successfully")
+    }else{
+        println("Failure during transfer. Try again!")
+    }
     println()
 
+    println("Balance Account - Maria")
+    println("Initial balance ${accountMaria.balance}")
+    accountMaria.deposit(30.0)
+    accountMaria.withdraw(20.0)
+    if(accountMaria.transfer(10.0, accountJohn)){
+        println("Transfer was completed successfully")
+    }else{
+        println("Failure during transfer. Try again!")
+    }
+    println()
+
+    printAccountInfo(accountJohn)
     printAccountInfo(accountMaria)
-    accountMaria.deposit(60.0)
-    accountMaria.withdraw(55.0)
-    accountMaria.withdraw(900.0)
-    println("balance ${accountMaria.balance}")
-
-
 
 }
 
@@ -36,87 +47,33 @@ fun printAccountInfo(account: Account){
     println()
 }
 
-fun testConditions(amount: Double){
-    if (amount > 0.0) {
-        println("positive balance")
-    } else if (amount == 0.0) {
-        println("neutral balance")
-    } else {
-        println("negative balance")
-    }
-
-    when {
-        amount > 0.0 -> println("positive balance")
-        amount == 0.0 -> println("neutral balance")
-        else -> println("negative balance")
-    }
-}
-
-fun testLoops(entity: String, account: Int, amount: Double){
-    for(i in 1..5){
-        println("entity $entity$i")
-        println("account ${account+i}")
-        println("amount ${amount+i}")
-    }
-
-    println("-------------------")
-
-    for(i in 5 downTo 1){
-        println("entity $entity$i")
-        println("account ${account+i}")
-        println("amount ${amount+i}")
-    }
-
-    println("-------------------")
-
-    for(i in 5 downTo 1 step 2){
-        println("entity $entity$i")
-        println("account ${account+i}")
-        println("amount ${amount+i}")
-    }
-
-    println("-------------------")
-    var contador = 0
-    while (contador < 5){
-        println("entity $entity$contador")
-        println("account ${account+contador}")
-        println("amount ${amount+contador}")
-        contador++
-    }
-}
-
-fun testCopyAndReference(){
-    var balance1 = 10.0
-    var balance2 = balance1
-    balance2 = 250.0
-    println("balance1 $balance1")
-    println("balance2 $balance2")
-
-    val account1 = Account()
-    account1.owner = "Test1"
-    val account2 = account1
-    account2.owner = "Test2"
-    println("account1 ${account1.owner}")
-    println("account2 ${account2.owner}")
-}
-
 class Account{
     var owner = ""
     var number = 1000
     var balance = 0.0
 
     fun deposit(value: Double){
-        println("After deposit money... $value")
+        println("After deposit $value")
         if(value > 0){
             balance += value
         }
     }
 
     fun withdraw(value: Double){
-        println("After withdraw money... $value")
+        println("After withdraw $value")
         if(value <= balance){
             balance -= value
         }
+    }
+
+    fun transfer(value: Double, destinationAccount: Account): Boolean{
+        println("After transfer $value to ${destinationAccount.owner}")
+        if(value <= balance){
+            balance -= value
+            destinationAccount.balance += value
+            return true
+        }
+        return false
     }
 
 }
